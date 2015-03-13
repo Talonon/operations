d6df0fd28a40b9a41858c2dd6fb19a1309d5d2b8
Andy Muth <andy@kps3.com>
2015-03-12 23:10:59 -0700

    Changes to how controller geters are handled

--- src/Kps3/Framework/Controllers/BaseController.php
+++ src/Kps3/Framework/Controllers/BaseController.php
@@ -8,55 +8,62 @@
     abstract class BaseController extends Controller {
 
       protected function getString($key, $required = false) {
-        $this->_requiredCheck($key, $required);
-        return \Input::get($key);
+        return $this->_get($key, $required, function($key) {
+          return \Input::get($key);
+        });
       }
 
       protected function getInt($key, $required = false) {
-        $this->_requiredCheck($key, $required);
-        $input = \Input::get($key);
-        $val = intval($input, 10);
-        if (is_numeric($input) && $val == $input) {
-          return $val;
-        } else {
-          $this->_invalidFormat($key);
-        }
+        return $this->_get($key, $required, function($key) {
+          $input = \Input::get($key);
+          if ($input == null) return null;
+          $val = intval($input, 10);
+          if (is_numeric($input) && $val == $input) {
+            return $val;
+          } else {
+            $this->_invalidFormat($key);
+          }
+        });
       }
 
       protected function getFloat($key, $required = false) {
-        $this->_requiredCheck($key, $required);
-        $input = \Input::get($key);
-        $val = floatval($input);
-        if (is_numeric($input) && $val == $input) {
-          return $val;
-        } else {
-          $this->_invalidFormat($key);
-        }
+        return $this->_get($key, $required, function($key) {
+          $input = \Input::get($key);
+          if ($input == null) return null;
+          $val = floatval($input);
+          if (is_numeric($input) && $val == $input) {
+            return $val;
+          } else {
+            $this->_invalidFormat($key);
+          }
+        });
       }
 
       protected function getBoolean($key, $required = false) {
-        $this->_requiredCheck($key, $required);
-        $input = \Input::get($key);
-        $input = strtolower($input);
-        switch ($input) {
-          case $input == true:
-          case $input == 1:
-          case 'true':
-          case 'on':
-          case 'yes':
-          case 'y':
-            return true;
-            break;
-          case $input === 0:
-          case $input === '0':
-          case 'false':
-          case 'off':
-          case 'no':
-          case 'n':
-            return false;
-          default:
-            $this->_invalidFormat($key);
-        }
+        return $this->_get($key, $required, function($key) {
+          $input = \Input::get($key);
+          if ($input == null) return null;
+          $input = strtolower($input);
+          switch ($input) {
+            case $input == true:
+            case $input == 1:
+            case 'true':
+            case 'on':
+            case 'yes':
+            case 'y':
+              return true;
+              break;
+            case $input === 0:
+            case $input === '0':
+            case 'false':
+            case 'off':
+            case 'no':
+            case 'n':
+              return false;
+            default:
+              $this->_invalidFormat($key);
+          }
+        });
       }
 
       /**
@@ -65,9 +72,10 @@
        * @return Carbon|null
        */
       protected function getDateTime($key, $format = null, $tz = null, $required = false) {
-        $this->_requiredCheck($key, $required);
-        $input = \Input::get($key);
-        return $input ? ($format ? Carbon::createFromFormat($format, $input) : Carbon::parse($input, $tz)) : null;
+        return $this->_get($key, $required, function($key) use ($format, $tz) {
+          $input = \Input::get($key);
+          return $input ? ($format ? Carbon::createFromFormat($format, $input) : Carbon::parse($input, $tz)) : null;
+        });
       }
 
       private function _badRequest($key) {
@@ -78,10 +86,11 @@
         throw new \InvalidArgumentException($key);
       }
 
-      private function _requiredCheck($key, $required) {
+      private function _get($key, $required, Callable $callback) {
         if ($required && !\Input::has($key)) {
           $this->_badRequest($key);
         }
+        $callback($key, $required);
       }
     }
 
