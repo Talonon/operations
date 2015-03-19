a7972ad643f0f9f8376f86e6fdb0ea507627d911
Andy Muth <andy@kps3.com>
2015-03-19 08:44:15 -0700

    Used correct exploder

--- src/Kps3/Framework/Exceptions/EntityNotFoundException.php
+++ src/Kps3/Framework/Exceptions/EntityNotFoundException.php
@@ -5,7 +5,7 @@
     class EntityNotFoundException extends InternalException {
 
       public function __construct($entityType, $id) {
-        $parts = explode(',', $entityType);
+        $parts = explode('\\', $entityType);
         $entityType = array_pop($parts);
         parent::__construct($entityType . ' not found with id ' . (is_array($id) ? print_r($id, true) : $id));
       }
