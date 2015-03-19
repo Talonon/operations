3077967b0eb84e487d14eef943f4a614aa2f6a19
Andy Muth <andy@kps3.com>
2015-03-19 07:58:47 -0700

    Fixed error message in the Not Found Entity

--- src/Kps3/Framework/Exceptions/EntityNotFoundException.php
+++ src/Kps3/Framework/Exceptions/EntityNotFoundException.php
@@ -5,7 +5,7 @@
     class EntityNotFoundException extends InternalException {
 
       public function __construct($entityType, $id) {
-        parent::__construct($entityType . ' not found with id ' . is_array($id) ? print_r($id, true) : $id);
+        parent::__construct($entityType . ' not found with id ' . (is_array($id) ? print_r($id, true) : $id));
       }
 
     }
