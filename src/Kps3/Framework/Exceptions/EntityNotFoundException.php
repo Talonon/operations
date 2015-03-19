109680defda016cb39a5ea917eec4ab7932c6d03
Andy Muth <andy@kps3.com>
2015-03-19 08:43:08 -0700

    Handling errors differently.

--- src/Kps3/Framework/Exceptions/EntityNotFoundException.php
+++ src/Kps3/Framework/Exceptions/EntityNotFoundException.php
@@ -5,6 +5,8 @@
     class EntityNotFoundException extends InternalException {
 
       public function __construct($entityType, $id) {
+        $parts = explode(',', $entityType);
+        $entityType = array_pop($parts);
         parent::__construct($entityType . ' not found with id ' . (is_array($id) ? print_r($id, true) : $id));
       }
 
