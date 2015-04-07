f39036f40205222cb5ac2bb51e6c96e24ef1eb2d
Andy Muth <andy@kps3.com>
2015-04-07 08:06:55 -0700

    Added enum base class.

---
+++ src/Kps3/Framework/Models/Enum.php
@@ -0,0 +1,21 @@
+<?php
+
+
+  namespace Kps3\Models {
+
+    use Kps3\Framework\Exceptions\InternalException;
+
+    abstract class Enum {
+
+      public static function GetValue($key) {
+        $key = strtoupper($key);
+        $oc = new \ReflectionClass(get_called_class());
+        $constants = $oc->getConstants();
+        if (array_key_exists($key, $constants)) {
+          return $constants[$key];
+        } else {
+          throw new InternalException('Invalid enumeration key ' . $key);
+        }
+      }
+    }
+  }
