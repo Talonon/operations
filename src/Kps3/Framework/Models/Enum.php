e37a749f416a6255a408f7995980fca89507cdac
Andy Muth <andy@kps3.com>
2015-08-31 10:28:39 -0700

    Updated composer to require Laravel as a dependency. Updated Enum class for checking to see if value is in the enumeration

--- src/Kps3/Framework/Models/Enum.php
+++ src/Kps3/Framework/Models/Enum.php
@@ -7,6 +7,12 @@
 
     abstract class Enum {
 
+      public static function IsValue($value) {
+        $oc = new \ReflectionClass(get_called_class());
+        $constants = $oc->getConstants();
+        return in_array($value, $oc->getConstants());
+      }
+
       public static function GetValue($key) {
         $key = strtoupper($key);
         $oc = new \ReflectionClass(get_called_class());
